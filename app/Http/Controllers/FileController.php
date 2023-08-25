5ffde5f9218d9edb7ce6bf488ece13c395a4d7e1
tarik bosunia <bosuniamdtarik005@gmail.com>
2023-08-25 09:34:39 +0600

    backend is ready to created folder

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreFolderRequest;
+use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class FileController extends Controller
@@ -10,4 +13,23 @@ class FileController extends Controller
     public function myFiles(){
         return Inertia::render('MyFiles');
     }
+
+    public function createFolder(StoreFolderRequest $request){
+        $data = $request->validated();
+        $parent = $request->parent;
+
+        if(!$parent){
+            $parent = $this->getRoot();
+        }
+
+        $file = new File();
+        $file->is_folder = 1;
+        $file->name = $data['name'];
+        $parent->appendNode($file);
+    }
+
+    private function getRoot(){
+        return File::query()->whereIsRoot()->where('created_by', Auth::id())->firstOrFail();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::controller(FileController::class)
     ->middleware(['auth', 'verified'])
     ->group(function () {
         Route::get('/my-files', 'myFiles')->name('myFiles');
+        Route::get('/folder/create', 'createFolder')->name('folder.create');
     });
 
 // Route::get('/dashboard', function () {
